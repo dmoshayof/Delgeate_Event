cdabd7bb688dabc445ebbc8b47e42227ec546b32
dmoshayof <43912232+dmoshayof@users.noreply.github.com>
2019-03-19 12:50:47 +0200

    classes

---
+++ ComposedMission.cs
@@ -0,0 +1,46 @@
+﻿      using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public class ComposedMission : IMission
+    {
+
+        public delegate double compodesDel(double value);
+        private IList<compodesDel> missions;
+        public string Name {get;}
+        public string Type { get; }
+        public ComposedMission( string name)
+        {
+            Name = name;
+            Type = "Composed Mission";
+            missions = new List<compodesDel>();
+        }
+        public ComposedMission Add(Func<double,double> func)
+        {
+            compodesDel del = new compodesDel(func);
+            missions.Add(del);
+            return this;
+        }
+       
+        
+
+        public event EventHandler<double> OnCalculate;
+
+        public double Calculate(double value)
+        {
+            
+            foreach( var i in missions)
+            {
+                value = i.Invoke(value);
+            }
+            OnCalculate?.Invoke(this, value);
+            return value;
+        }
+
+     
+    }
+}
---
+++ MissionContainer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+
+    public class FunctionsContainer
+    {
+        private Dictionary<string, Func<double, double>> funcPairs { get; set; }
+
+        public FunctionsContainer()
+        {
+            funcPairs = new Dictionary<string, Func<double, double>>();
+        }
+
+        public Func<double,double> this[string name]
+        {
+            get {
+                if (funcPairs.ContainsKey(name)){
+                    return funcPairs[name];
+                }
+                else
+                {
+                    return val=> val;
+                }
+
+                }
+            
+            set => funcPairs[name] = value;
+
+        }
+
+
+        public IList<string> getAllMissions()
+        {
+            IList<string> keys = funcPairs.Keys.ToList();
+            return keys;
+        }
+    }
+}
